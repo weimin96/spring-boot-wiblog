69c9921be41a51db2d2aec743ad887043481d45d
潘维敏 <panwm@sina.com>
2021-01-22 18:16:03 +0800

    日志监控

--- src/main/java/com/wiblog/core/controller/AdminController.java
+++ src/main/java/com/wiblog/core/controller/AdminController.java
@@ -19,10 +19,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 管理界面控制层
