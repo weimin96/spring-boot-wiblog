81cda52dd26d147845435e7cbc8054c705364596
潘维敏 <panwm@sina.com>
2021-01-26 18:01:03 +0800

    添加评论定位跳转

--- src/main/java/com/wiblog/core/controller/CommentController.java
+++ src/main/java/com/wiblog/core/controller/CommentController.java
@@ -1,5 +1,7 @@
 package com.wiblog.core.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.wiblog.core.aop.RequestRequire;
 import com.wiblog.core.common.BaseController;
 import com.wiblog.core.common.RoleEnum;
@@ -8,11 +10,14 @@ import com.wiblog.core.entity.Comment;
 import com.wiblog.core.entity.User;
 import com.wiblog.core.service.ICommentService;
 import com.wiblog.core.service.IUserRoleService;
+import com.wiblog.core.utils.DateUtil;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 评论控制层
@@ -119,9 +124,9 @@ public class CommentController extends BaseController {
      */
     @GetMapping("/getUserComment")
     public ServerResponse<?> getUserComment(HttpServletRequest request, Long uid, String type,
-                                         @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
-                                         @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize,
-                                         @RequestParam(value = "orderBy", defaultValue = "desc") String orderBy) {
+                                            @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
+                                            @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize,
+                                            @RequestParam(value = "orderBy", defaultValue = "desc") String orderBy) {
         User user = getLoginUser(request);
         boolean isPermit = false;
         // 用户本身 或 管理员
@@ -129,5 +134,39 @@ public class CommentController extends BaseController {
             isPermit = true;
         }
         return commentService.getUserComment(uid, pageNum, pageSize, orderBy, isPermit, type);
+    }
+
+    /**
+     * 评论定位
+     *
+     * @param id id
+     * @return ServerResponse
+     */
+    @GetMapping("getCommentById")
+    public ServerResponse<?> getCommentById(String id) {
+        Map<String, Object> result = new HashMap<>();
+        // 获取评论信息
+        Comment comment = commentService.getById(id);
+        int pageNum;
+        String type;
+        // 评论文章
+        if (comment.getGenId() == 0L) {
+            // 评论页码
+            pageNum = comment.getFloor() / 10 + 1;
+            type = "root";
+        } else {
+            // 回复评论
+            Comment rootComment = commentService.getById(comment.getGenId());
+            pageNum = rootComment.getFloor() / 10 + 1;
+            type = "sub";
+            LambdaQueryWrapper<Comment> queryWrapper = new QueryWrapper<Comment>().lambda();
+            // 同一评论下的评论数
+            queryWrapper.eq(Comment::getGenId, rootComment.getId()).apply("date_format(create_time,'%Y-%m-%d %H:%i:%S') < {0}", DateUtil.formatSimpleDate(comment.getCreateTime()));
+            int count = commentService.count(queryWrapper);
+            result.put("count", count);
+        }
+        result.put("pageNum", pageNum);
+        result.put("type", type);
+        return ServerResponse.success(result);
     }
 }
